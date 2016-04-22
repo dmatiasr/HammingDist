9f69ce67c458898129f34abfd5ebdc626ad6f6df
MauriDV <mdellevedove12@gmail.com>
2016-04-22 02:26:15 -0300

    Ahora, creo, que genera todas las permutaciones por indice. Agregue funcionalidades

--- hamming.hs
+++ hamming.hs
@@ -1,16 +1,40 @@
+--CONCATENA 2 PALABRAS DECREMENTANDO UN INDICE
 concatenation :: String -> String -> String
 concatenation x (y:ys) = x++(drop (length(x)) (y:ys)) 
 
+--GENERA PERMUTACIONES POR INDICE ENTRE DOS PALABRAS
 wordGen :: String -> String -> [String]
 wordGen [] (y:ys) = [] 
 wordGen (x:xs) (y:ys) = [concatenation (x:xs) (y:ys)] ++ (wordGen (init(x:xs)) (y:ys) )
 
-aux :: [[Char]] -> String
-aux (x:xs) = x
+--GENERA PERMUTACIONES POR INDICE DESDE UNA LISTA DE STRINGS Y UN STRINGS 
+hammiList::[String]->String->[String]
+hammiList [] s = []
+hammiList (x:xs) s = wordGen x s ++ hammiList xs s
 
+--RETORNA TODAS LAS PERMUTACIONES POR INDICE ENTRE DOS PALABRAS
 hammi :: String -> String -> [String]
-hammi w1 w2 = h1++h2++h3++h4
+hammi [] [] = []
+hammi w1 w2 = rm(h1++h2++hammiList h1 w1++hammiList h2 w2)
 			where 	h1 = wordGen w1 w2;
 					h2 = wordGen w2 w1;
-					h3 = wordGen (aux(drop (length(w1)-1) (h1))) (aux(drop (length(w2)-1) (h2)));
-					h4 = wordGen (aux(drop (length(w2)-1) (h2))) (aux(drop (length(w1)-1) (h1)));
+
+--ELIMINA REPETIDOS DE UNA LISTA
+rm::[String]->[String]
+rm [] = []
+rm (x:xs) | any (==x) (xs) = rm xs
+		  | otherwise = [x] ++ rm xs
+
+--RETORNA DISTANCIA DE HAMMING ENTRE DOS PALABRAS
+wordDistHamming::String->String->Int
+wordDistHamming [] [] = 0
+wordDistHamming (x:xs) (y:ys) | x==y = 0+wordDistHamming xs ys
+							  |	otherwise = 1+wordDistHamming xs ys
+
+--RETORNA UNA LISTA CON TODAS LAS DISTANCIAS DE HAMMING DE UNA LISTA DE STRINGS Y UNA STRING
+distHamming::[String]->String->[Int]
+distHamming [] s = []
+distHamming (x:xs) s = [wordDistHamming x s]++distHamming xs s
+
+
+
